879fda01b531efdf7b4459a5b4030c88e598390f
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-01-19 16:20:57 +0530

    Update SearchTest.java

--- src/test/java/com/testautomationguru/container/test/SearchTest.java
+++ src/test/java/com/testautomationguru/container/test/SearchTest.java
@@ -22,7 +22,7 @@ public class SearchTest extends BaseTest {
     @Test()
     public void googleTest() {
         google.goTo();
-        Assert.assertTrue(google.checkTitle("G"));
+        Assert.assertTrue(google.checkTitle("g"));
         
    
     }
