fced3ae3ab5d2e98ac3f179a85edd8c32dfa77fa
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-01-19 10:38:31 +0530

    Update SearchPage.java

--- src/main/java/com/testautomationguru/container/pages/SearchPage.java
+++ src/main/java/com/testautomationguru/container/pages/SearchPage.java
@@ -15,18 +15,6 @@ public class SearchPage {
     private final WebDriver driver;
     private final WebDriverWait wait;
 
-    @FindBy(name = "q")
-    private WebElement searchBox;
-
-    @FindBy(css = "input.lsb")
-    private WebElement searchButton;
-
-    @FindBy(className = "rc")
-    private List<WebElement> searchResults;
-
-    @FindBy(id = "foot")
-    private WebElement footer;
-
     public SearchPage(final WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
@@ -38,20 +26,8 @@ public class SearchPage {
         System.out.println("Browser launched and navigated to Google");
     }
 
-    public void searchFor(String text) {
-        this.searchBox.sendKeys(text);
-        System.out.println(text + " - entered for search");
-        wait.until(ExpectedConditions.elementToBeClickable(this.searchButton));
-        System.out.println("Search button clicked");
-        this.searchButton.click();
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.className("rc")));
-        System.out.println("Results appeared");
-    }
-
-    public List<WebElement> getResults() {
-        System.out.println("Results Count : " + this.searchResults.size());
-        System.out.println("---------------------------------------------");
-        return this.searchResults;
-    }
+   public Boolean checkTitle(String expectedTitle){
+       return driver.getTitle().contains(expectedTitle);
+   }
 
 }
