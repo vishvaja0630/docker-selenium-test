31506ac218ae5e1ded1bbef5337120ff19a75bbb
vishvaja0630 <37453721+vishvaja0630@users.noreply.github.com>
2021-01-20 10:22:15 +0530

    Update SearchPage.java

--- src/main/java/com/testautomationguru/container/pages/SearchPage.java
+++ src/main/java/com/testautomationguru/container/pages/SearchPage.java
@@ -21,13 +21,22 @@ public class SearchPage {
         this.wait = new WebDriverWait(driver, 30);
     }
 
-    public void goTo() {
+    /*public void goTo() {
         this.driver.get("https://www.google.com");
         System.out.println("Browser launched and navigated to Google");
     }
-
-   public Boolean checkTitle(String expectedTitle){
+    
+    public Boolean checkTitle(String expectedTitle){
        return driver.getTitle().contains(expectedTitle);
-   }
+   }*/
+  
+  public void checkTitle(){
+  driver.navigate().to("https://www.google.com");
+  String pageTitle = driver.getTitle();
+  System.out.println(pageTitle);
+  }
+                       
+  driver.close();
+  driver.quit();
 
 }
